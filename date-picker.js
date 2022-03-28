76fa88d834b80ec4a201de069a6df2b5e37cea45
limbo-yan <46770483+limbo-yan@users.noreply.github.com>
2022-03-28 00:36:02 -0400

    Finished rough format, single date modifier

--- date-picker.js
+++ date-picker.js
@@ -3,6 +3,7 @@ import {BPDimens, BPStandards} from '../../../utils/business-process/standards';
 
 import BPTextInput from './text-input';
 
+import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import StaticDateTimePicker from '@mui/lab/StaticDateTimePicker';
@@ -10,7 +11,7 @@ import {ClickAwayListener, Popper} from '@mui/material';
 
 import {dateOptions} from '../../../utils/business-process/date-options';
 
-export const BPDatePicker = ({label, onChange}) => {
+export const BPDatePicker = ({label, onChange, baseDate, callBack}) => {
   // Date picker value.
   const [value, setValue] = useState(null);
 
@@ -27,9 +28,118 @@ export const BPDatePicker = ({label, onChange}) => {
     setIsOpen(false);
   };
 
+  const addAction = (base, amount, flag) => {
+    switch (flag) {
+      case 'h':
+        const newhour = parseInt(base.getHours()) + parseInt(amount);
+        base.setHours(newhour);
+        break;
+      case 'd':
+        const newdate = parseInt(base.getDate()) + parseInt(amount);
+        base.setDate(newdate);
+        break;
+      case 'm':
+        const newmin = parseInt(base.getMinutes()) + parseInt(amount);
+        base.setMinutes(newmin);
+        break;
+      case 'mo':
+        const newmonth = parseInt(base.getMonth()) + parseInt(amount);
+        base.setMonth(newmonth);
+        break;
+      case 'y':
+        const newyear = parseInt(base.getFullYear()) + parseInt(amount);
+        base.setFullYear(newyear);
+        break;
+    }
+  };
+  const minusAction = (base, amount, flag) => {
+    switch (flag) {
+      case 'h':
+        const newhour = parseInt(base.getHours()) - parseInt(amount);
+        base.setHours(newhour);
+        break;
+      case 'd':
+        const newdate = parseInt(base.getDate()) - parseInt(amount);
+        base.setDate(newdate);
+        break;
+      case 'm':
+        const newmin = parseInt(base.getMinutes()) - parseInt(amount);
+        base.setMinutes(newmin);
+        break;
+      case 'mo':
+        const newmonth = parseInt(base.getMonth()) - parseInt(amount);
+        base.setMonth(newmonth);
+        break;
+      case 'y':
+        const newyear = parseInt(base.getFullYear()) - parseInt(amount);
+        base.setFullYear(newyear);
+        break;
+    }
+  };
+
   // Process the input value into datepicker value.
   useEffect(() => {
-    // TODO
+    const changeFlag = false;
+    console.log(baseDate);
+    const updatedDate = new Date(baseDate);
+
+    // console.log(baseDate);
+    if (inputValue.includes('-') || inputValue.includes('+')) {
+      if (inputValue.length >= 4) {
+        const reviseList = inputValue.split(' ');
+        reviseList.forEach((action) => {
+          // console.log(updatedDate);
+          const amount = action.charAt(1);
+          const flag = action.substring(2);
+          if (action.charAt(0) == '+') {
+            addAction(updatedDate, amount, flag);
+            changeFlag = true;
+          } else {
+            minusAction(updatedDate, amount, flag);
+            changeFlag = true;
+          }
+        });
+      }
+    }
+    // console.log(updatedDate);
+    if (changeFlag) {
+      setValue(updatedDate);
+      return;
+    }
+
+    const input = inputValue;
+    // if (inputValue.includes('-') || inputValue.includes('+')) {
+
+    // }
+    const inputValueArr = input.split(/[:\s]/i);
+
+    const parsedValue = inputValueArr[0];
+    const hour = '00';
+    const minutes = '00';
+
+    if (inputValueArr.length > 1) {
+      hour = inputValueArr[1].replace(/\D/g, '');
+      parsedValue += ' ' + hour;
+    }
+    if (inputValueArr.length > 2) {
+      minutes = inputValueArr[2].replace(/\D/g, '');
+      if (/^\d+$/.test(minutes)) {
+        parsedValue += ':' + minutes;
+      }
+    }
+    if (input.toLowerCase().includes('am')) {
+      if (! /^\d+$/.test(minutes)) {
+        parsedValue += ':' + '00';
+      }
+      parsedValue += ' AM';
+    } else if (input.toLowerCase().includes('pm')) {
+      if (! /^\d+$/.test(minutes)) {
+        parsedValue += ':' + '00';
+      }
+      parsedValue += ' PM';
+    }
+
+    setValue(parsedValue);
   }, [inputValue]);
 
   // Process the datepicker value into input value.
@@ -123,12 +233,14 @@ export const BPDatePicker = ({label, onChange}) => {
                     openTo="day"
                     value={value}
                     onChange={(newValue) => {
+                      // console.log(newValue);
                       setValue(newValue);
+                      callBack(newValue);
                     }}
                     onAccept={() => {
                       handlePopoverClose();
                     }}
-                    renderInput={(params) => <></>}
+                    renderInput={(params) => <TextField {...params}/>}
                   />
                 </LocalizationProvider>
               </div>
