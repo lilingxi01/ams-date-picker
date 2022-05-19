2d05505a3949dddccfea8cb4337cfa21b17fe8b8
Lingxi Li <lilingxi01@gmail.com>
2022-05-19 18:26:55 -0400

    [Core] (WIP)

--- src/lib/date-picker-input.js
+++ src/lib/date-picker-input.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { parseDate } from './support/date-picker-processor.js';
 
 export const AmsDatePickerInput = ({
   id,
   value,
+  baseDate,
   onChange,
   onFocus,
   onBlur,
@@ -14,6 +16,24 @@ export const AmsDatePickerInput = ({
   useEffect(() => {
     setInputValue(value);
   }, [value]);
+
+  const onInputFinish = (text) => {
+    try {
+      const parsedDate = parseDate(text, baseDate || new Date());
+      if (onChange) {
+        onChange(parsedDate,);
+      }
+    } catch (e) {
+      onChange(null, e.message); // Return error message.
+    }
+  };
+
+  const isValidOnBlur = () => {
+    return (
+      inputValue.length > 0 &&
+      !inputValue.match(/^\d{1,2}\/\d{1,2}\/\d{4}, \d{1,2}:\d{2}(?::\d{2})? (?:AM|PM)?$/)
+    );
+  };
 
   return (
     <input
