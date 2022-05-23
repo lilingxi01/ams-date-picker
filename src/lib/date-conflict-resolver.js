364d42ef6ca1ddc26f04d32290b7141322c19545
Lingxi Li <lilingxi01@gmail.com>
2022-05-22 23:26:06 -0400

    [DSCR] (WIP) Migrating the core component of DSCR module.

--- src/lib/date-conflict-resolver.js
+++ src/lib/date-conflict-resolver.js
@@ -1,12 +1,137 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import { styled } from '@stitches/react';
 import { isInDaylightSavingConflictTime } from './support/date-picker-processor.js';
+import { IconAlertTriangle, IconCheck } from '@tabler/icons';
+import { AmsDesign } from './utils/standards.js';
 
-export const AmsDateConflictResolver = () => {
+/* -----------------------------------------------------------------------------
+ * Ams Date Conflict Resolver
+ * ---------------------------------------------------------------------------*/
+
+const AmsDateConflictResolverOption = ({
+  option,
+  design,
+}) => {
+  const { dateState, options } = useContext(DSCRContext);
+  const OptionComponent = option;
+  const optionName = Option.OPTION_NAME ?? 'unknown';
+  const isActive = dateState && options[optionName] === dateState.getTimezoneOffset();
+  const displayTime = dateState.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
   return (
-    <div>
-      {/* TODO: Implement this component. */}
-    </div>
+    <OptionComponent
+      style={{
+        width: '50%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        rowGap: '2px',
+        padding: '6px',
+        userSelect: 'none',
+        cursor: 'pointer',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          columnGap: '2px',
+          fontSize: 12,
+          color: isActive
+            ? AmsDesign.color.white
+            : (design?.accentColor ?? AmsDesign.color.accentColor),
+          fontWeight: '500',
+        }}
+      >
+        <IconCheck
+          width={13}
+          height={13}
+          strokeWidth={2.8}
+          style={{
+            marginTop: '1px',
+            display: isActive
+              ? 'block'
+              : 'none',
+          }}
+        />
+        <span>
+          {optionName === 'ams-dscr-earlier-option' ? 'Earlier' : 'Latter'}
+        </span>
+      </div>
+      <div
+        style={{
+          fontSize: 15,
+          color: isActive ? AmsDesign.color.white : AmsDesign.color.black,
+          fontWeight: '500',
+        }}
+      >
+        {displayTime}
+      </div>
+      <div
+        style={{
+          fontSize: 12,
+          color: isActive ? AmsDesign.color.white : AmsDesign.color.black,
+          fontWeight: '500',
+          opacity: isActive ? 0.70 : 0.55,
+        }}
+      >
+        {options && options[optionName] ? `UTC${options[optionName] / -60}` : 'Unknown'}
+      </div>
+    </OptionComponent>
+  );
+};
+
+export const AmsDateConflictResolver = ({ date, onChange, design }) => {
+  return (
+    <AmsDSCRModule.Root
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+      date={date}
+      onChange={onChange}
+    >
+      <div
+        style={{
+          width: '100%',
+          fontSize: 12,
+          fontWeight: '500',
+          padding: '6px 8px',
+          color: AmsDesign.color.gray[500],
+          borderBottom: `1px solid ${AmsDesign.color.gray[150]}`,
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+          columnGap: '4px',
+        }}
+      >
+        <IconAlertTriangle
+          width={13}
+          height={13}
+          strokeWidth={2.5}
+          style={{
+            marginTop: '1px',
+          }}
+        />
+        <span>Potential DLS Conflict</span>
+      </div>
+      <div
+        style={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
+        <AmsDateConflictResolverOption option={AmsDSCRModule.EarlierOption} />
+        <AmsDateConflictResolverOption option={AmsDSCRModule.LatterOption} />
+      </div>
+    </AmsDSCRModule.Root>
   );
 };
 
@@ -16,10 +141,7 @@ export const AmsDateConflictResolver = () => {
 
 const DSCRContext = React.createContext('ams-dscr');
 
-const DSCRRootContainer = styled(DSCRContext.Provider, {
-  display: 'flex',
-  flexDirection: 'row',
-});
+const DSCRRootContainer = styled(DSCRContext.Provider, {});
 
 const DSCROptionContainer = styled('div', {});
 
@@ -72,6 +194,7 @@ const DSCRRoot = ({ date, onChange, style, children, ...props }) => {
       }}
       value={{
         dateState: date,
+        options: [earlierOption, latterOption],
         onOptionClick: (optionIndex) => {
           getDateBasedOnOptionIndex(optionIndex);
         },
@@ -108,10 +231,12 @@ const DSCROption = ({ index, onClick, children, style, ...props }) => {
 const DSCREarlierOption = (props) => (
   <DSCROption index={0} {...props} />
 );
+DSCREarlierOption.OPTION_NAME = 'ams-dscr-earlier-option';
 
 const DSCRLatterOption = (props) => (
   <DSCROption index={1} {...props} />
 );
+DSCRLatterOption.OPTION_NAME = 'ams-dscr-latter-option';
 
 export const AmsDSCRModule = {
   Root: DSCRRoot,
--- src/lib/utils/standards.js
+++ src/lib/utils/standards.js
@@ -1,5 +1,25 @@
 const AmsColor = {
   // TODO
+
+  accentColor: '#0688ff',
+  white: '#fff',
+  black: '#000',
+
+  gray: {
+    30: '#fcfdfd',
+    50: '#fafbfb',
+    70: '#f7f8f8',
+    100: '#f3f4f6',
+    150: '#ebedf0',
+    200: '#e5e7eb',
+    300: '#d1d5db',
+    400: '#9ca3af',
+    500: '#6b7280',
+    600: '#4b5563',
+    700: '#374151',
+    800: '#1f2937',
+    900: '#111827',
+  },
 };
 
 const AmsDimension = {
