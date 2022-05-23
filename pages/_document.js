98ad412dafd97a0e5f10af0d03fcdc05a0c845ad
Lingxi Li <lilingxi01@gmail.com>
2022-05-23 16:28:47 -0400

    [General] Add Inter font.

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,10 @@ const AmsExampleDocument = () => {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
         <meta charSet="utf-8"/>
       </Head>
       <body>
