86abcad3d021411960a534b30a66ac42dd2ead5f
Lingxi Li <lilingxi01@gmail.com>
2022-05-25 13:32:05 -0400

    [Vercel] Add `should-preview` script.

---
+++ should-preview.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+## Do not automatically trigger build when pull-requesting into `deploy` branch.
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then
+  # Proceed with the build.
+    echo "✅ - Preview build can proceed"
+  exit 1;
+
+else
+  # Don't build.
+  echo "🛑 - Preview build cancelled"
+  exit 0;
+fi
