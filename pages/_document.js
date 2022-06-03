7206586ec49f347fdb8179fab6c40709dd06712a
Lingxi Li <lilingxi01@gmail.com>
2022-06-03 16:37:47 -0400

    [Website] Update demo.

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 import { getCssText } from '../packages/support/stitches.config';
 
 const AmsExampleDocument = () => {
-  const styles = getCssText();
   return (
     <Html>
       <Head>
-        <style id="stitches" dangerouslySetInnerHTML={{ __html: styles }} />
+        <style id="stitches" dangerouslySetInnerHTML={ { __html: getCssText() } } />
         <link
           rel="preconnect"
           href="https://fonts.googleapis.com"
@@ -17,7 +16,7 @@ const AmsExampleDocument = () => {
           href="https://fonts.gstatic.com"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Fira+Code&family=IBM+Plex+Sans:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap"
           rel="stylesheet"
         />
         <link
