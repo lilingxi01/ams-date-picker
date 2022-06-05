5bb3b63f5e0e64617c6c12b06b1d4524e2f31fad
Lingxi Li <lilingxi01@gmail.com>
2022-06-04 21:46:20 -0400

    [General] Optimize remote font import reliability.

--- pages/_document.js
+++ pages/_document.js
@@ -3,31 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 import { getCssText } from '../packages/support/stitches.config';
 
 const AmsExampleDocument = () => {
-  const styles = getCssText();
   return (
     <Html>
       <Head>
-        <style id="stitches" dangerouslySetInnerHTML={{ __html: styles }} />
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap"
-          rel="stylesheet"
-        />
+        <style id="stitches" dangerouslySetInnerHTML={ { __html: getCssText() } } />
         <meta charSet="utf-8"/>
       </Head>
       <body>
