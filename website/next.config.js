85b9e809a2cf787432a286d425e4ce1a5f9a321e
Lingxi Li <lilingxi01@gmail.com>
2023-02-16 20:55:28 -0500

    [WIP]

--- website/next.config.js
+++ website/next.config.js
@@ -10,6 +10,27 @@ const nextConfig = {
       'user-images.githubusercontent.com',
     ],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
-module.exports = withTM(nextConfig);
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
+module.exports = withMDX(withTM(nextConfig));
